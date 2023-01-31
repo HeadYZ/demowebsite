cc01bc6b24675876ef2e970e27f12411eb143e2c
Patryk <krasowski.p@outlook.com>
2023-01-31 22:11:46 +0100

    changed toggleNav function

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,27 @@
 
 const navBtn = document.querySelector('.navbar__btn')
 const navList = document.querySelector('.navbar__list')
-const body = document.querySelector('body')
 const navItems = document.querySelectorAll('.navbar__list-item')
 const allSections = document.querySelectorAll('section')
-
-
-
 
 //footer
 
 const footerDate = document.querySelector('.footer__date')
-
-
-
 
 window.onscroll = () => {
 	allSections.forEach(sec => {
 		let top = window.scrollY
 		let offset = sec.offsetTop - 100
 		let height = sec.offsetHeight
-	
+
 		let id = sec.getAttribute('id')
 
-	if (top >= offset && top < offset + height) {
-		navItems.forEach(links => {
-			links.classList.remove('active')
-			document.querySelector('a[href*=' + id + ']').parentElement.classList.add('active')
-		})
-	}
+		if (top >= offset && top < offset + height) {
+			navItems.forEach(links => {
+				links.classList.remove('active')
+				document.querySelector('a[href*=' + id + ']').parentElement.classList.add('active')
+			})
+		}
 	})
 }
 
@@ -40,18 +33,19 @@ const yearUpdate = () => {
 
 const toggleNav = () => {
 	navBtn.classList.toggle('active')
+	document.body.classList.toggle('sticky-body')
 
 	if (navBtn.classList.contains('active')) {
 		navList.classList.add('show-menu')
-		body.classList.add('sticky-body')
 	} else {
 		navList.classList.remove('show-menu')
-		body.classList.remove('sticky-body')
 	}
 }
 
 navItems.forEach(item => {
-	item.addEventListener('click', toggleNav)
+	item.addEventListener('click', () => {
+		toggleNav(), document.body.classList.remove('sticky-body')
+	})
 })
 
 navBtn.addEventListener('click', toggleNav)
